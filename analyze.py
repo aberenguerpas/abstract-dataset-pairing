bc7bcaeb51318dd6b20b02a137d08078a85c002f
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2023-01-21 17:23:38 +0100

    Update Readme

--- analyze.py
+++ analyze.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import argparse
 
 def main():
-    df = pd.read_csv('./results_w2v.csv')
-    print(df.mean(axis=0))
+    parser = argparse.ArgumentParser(description='Analyze results')
+    parser.add_argument('-i', '--input', default='results/', help='Name of the input folder storing CSV tables')
 
+    args = parser.parse_args()
+    df = pd.read_csv(args.input)
+
+    print(df.mean(axis=0))
 
 if __name__ == "__main__":
     main()
