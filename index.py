f25e9a096d44903afae74ae6c58f7165e5da19f1
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2023-01-30 11:50:12 +0100

    reduce batch size

--- index.py
+++ index.py
@@ -24,7 +24,7 @@ def proccessHeaders(headers):
     headers = headers.lower()
     headers = headers.replace('&nbsp;',' ')
     # The maximum token length admitted by is 256
-    max_sequence_length = 128
+    max_sequence_length = 64
     # Larger texts are cut into 256 token length pieces and their vectors are averaged
     # Take into account that a cell could have more than one token
     if len(headers) > max_sequence_length:
@@ -42,7 +42,7 @@ def proccessText(text):
     text = text.lower()
 
     # The maximum token length admitted by is 256
-    max_sequence_length = 128
+    max_sequence_length = 64
     # Larger texts are cut into 256 token length pieces and their vectors are averaged
     # Take into account that a cell could have more than one token
     if len(text) > max_sequence_length:
