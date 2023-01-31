ee404c6caca17b6c9f6c55bd41ca51e1c00e87af
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2023-01-31 16:55:38 +0100

    Sorted List

--- index.py
+++ index.py
@@ -67,7 +67,7 @@ def main():
 
     files = os.listdir(files_path)
 
-    files = [i for i in files if i.endswith(".csv")]
+    files = [i for i in files if i.endswith(".csv")].sort()
 
     invertedIndex = dict()
     if args.model == 'sci' or args.model == 'brt':
