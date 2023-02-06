9dab6531f21cb06b5a7571b9d2884e2b24aa012b
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2023-02-06 09:38:34 +0100

    Check headers script

---
+++ test2.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+files_path = './data/'
+files = os.listdir(files_path)
+
+files = [i for i in files if i.endswith(".csv")]
+files.sort()
+
+for file in files:
+            # Headers
+            print(file)
+            df = pd.read_csv(files_path+file , encoding = "ISO-8859-1", on_bad_lines='skip', engine='python', sep = None, nrows=1020)
+            print(df.columns.values)
