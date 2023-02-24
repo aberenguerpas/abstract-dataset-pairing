8a8360c8cfc702300af485369341ad0da3dee322
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2023-02-24 11:09:43 +0100

    check time

--- classify.py
+++ classify.py
@@ -43,7 +43,7 @@ def search(vec_abstract, index_h, index_c, inverted, alpha):
     # Headers search
     start = time.time()
     distances_h, indices_h = index_h.search(vec_abstract, 20) #index_h.ntotal - 1
-    print(start - time.time())
+    print(time.time()- start)
     results_h = [(inverted[r], distances_h[0][i]) for i, r in enumerate(indices_h[0])]
     ids_list += [k for k,_ in results_h]
 
